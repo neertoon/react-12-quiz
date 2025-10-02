66859e64283c0b16b7bff532e1aebfe1f5461132
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2025-10-02 22:23:21 +0200

    Zbudowanie komponentu Quiz z pytaniem i przyciskami

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
+
 function App() {
-    return <Header></Header>
+    return <>
+        <Header></Header>
+        <main>
+        <Quiz ></Quiz>
+        </main>
+        </>
 }
 
 export default App;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,26 @@
 import { useState } from 'react';
+import QUESTIONS from "../questions.js";
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-
     const [userAnswers, setUserAnswers] = useState([]);
 
-    return <p>Currently active Question {activeQuestionIndex}</p>
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((previewAnswers) => { return [...previewAnswers, selectedAnswer] } );
+    }
+
+    return (
+    <div id="quiz">
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text} </h2>
+            <ul id="answers">
+                {QUESTIONS[activeQuestionIndex].answers.map(
+                    (answer) => <li key={answer} class="answer">
+                    <button onclick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                )}
+            </ul>
+        </div>
+    </div>
+    );
 }
