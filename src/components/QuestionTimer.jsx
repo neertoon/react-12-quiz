76d0de9bc531445df67e4c972a5b73b17aecac06
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2026-01-01 15:42:59 +0100

    StrictMode odpala każdy komponent 2 razy do wykrycia błedów - dlatego potrzeba cleanup function. Return w useEffect odpala się gdy komponent jest czyszczony

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -3,11 +3,19 @@ export default function QuestionTimer({ timeout, onTimeout }) {
     const [timeLeft, setTimeLeft] = useState(timeout);
 
     useEffect(() => {
-        setTimeout(onTimeout, timeout);
+        const timer = setTimeout(onTimeout, timeout);
+
+        return () => {
+            clearTimeout(timer)
+        };
     }, [onTimeout, timeout]);
 
     useEffect(() => {
-        setInterval(() => setTimeLeft((prevLeftTime) => prevLeftTime - 100), 100);
+        const interval = setInterval(() => setTimeLeft((prevLeftTime) => prevLeftTime - 100), 100);
+
+        return () => {
+            clearInterval(interval)
+        };
     }, []);
 
 
